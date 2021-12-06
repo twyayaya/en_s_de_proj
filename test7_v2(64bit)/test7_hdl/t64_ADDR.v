e6630ebdc09a0edaf7d461a26088c75d1fee3f5f
twyayaya <69721913+twyayaya@users.noreply.github.com>
2021-12-06 08:11:37 +0800

    output "reg" R_ADDRr -> output [13:0] R_ADDRr

--- test7_v2(64bit)/test7_hdl/t64_ADDR.v
+++ test7_v2(64bit)/test7_hdl/t64_ADDR.v
@@ -5,15 +5,21 @@ module t64_ADDR (input [13:0] R_ADDR,
     input  R_EN,
     input [63:0] real_data, 
     input [63:0] wrong_real_data,
-    output reg  [13:0] R_ADDRr
+    output [13:0] R_ADDRr
 );
 
+reg [13:0] R_ADDRrr;
+
+
+assign R_ADDRr = R_ADDRrr;
+
+
 always @(real_data or wrong_real_data) begin
-    R_ADDRr = 0;
+    R_ADDRrr = 0;
     if(R_EN )begin
-        R_ADDRr = 0;
+        R_ADDRrr = 0;
         if(real_data != wrong_real_data)begin
-            R_ADDRr = R_ADDR;
+            R_ADDRrr = R_ADDR;
         end
     end
     
