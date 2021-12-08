d0cdeeec33fffcd5374c9bb2d1c963275723e7f8
twyayaya <69721913+twyayaya@users.noreply.github.com>
2021-12-09 00:01:53 +0800

    Update t64_ADDR_new.v

--- get_address_new/t64_ADDR_new.v
+++ get_address_new/t64_ADDR_new.v
@@ -24,7 +24,7 @@ always @(R_EN or wrong_real_data) begin
         R_ADDRrr = 0;
         if( (SB_CORRECT != DB_DETECT) && (SB_CORRECT ==1) )begin // singel error , will be corrected,  ( || (real_data != wrong_real_data) )
             R_ADDRrr = R_ADDR;
-            errors = wrong_real_data;
+            errors = real_data; //has problem
         end
         if( (SB_CORRECT == 1) &&  (DB_DETECT == 1) )begin //Multiple errors NOT corrected  =>  real  data = wrong data
             R_ADDRrr = R_ADDR;
